b27431e9f7bd6a14f28247c69d931cb961f39ccc
nigist <niye2121@gmail.com>
2023-12-05 16:04:03 +0300

    Basic Design

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import {AiOutlineDelete} from 'react-icons/ai';
+import { FaCheck } from 'react-icons/fa';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
   return (
     <div className="App">
       <h1>My Todos</h1>
@@ -17,21 +21,30 @@ function App() {
             <input type="text" placeholder="What's the description of your todo app?"/>
           </div>
           <div className="todo-input-item">
-            <button type="button" classname="primaryBtn">
+            <button type="button" className="primary-btn">
               Add
             </button>
           </div>
         </div>
 
         <div className="btn-area">
-          <button>Todo</button>
-          <button>Completed</button>
+          <button className={`secondery-btn ${isCompleteScreen===false && 'active'}`}  onClick={()=>setIsCompleteScreen(false)}>Todo</button>
+          <button className={`secondery-btn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}>Completed</button>
         </div>
 
         <div className="todo-list">
           <div className="todo-list-item">
+            <div>
             <h3>Task 1</h3>
             <p>Description</p>
+            </div>
+            <div>
+              <AiOutlineDelete className='icon'/>
+              <FaCheck  className='check-icon'/>
+
+
+            </div>
+           
           </div>
         </div>
 
