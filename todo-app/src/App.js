dd4dd70d214a9c4c7f537b3f4ab932831a2f1296
nigist <niye2121@gmail.com>
2023-12-05 12:08:01 +0300

    todo app structure

---
+++ todo-app/src/App.js
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todos</h1>
+
+      <div className="todo-wrapper">
+        <div className="todo-input">
+          <div className="todo-input-item">
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?"/>
+          </div>
+          <div className="todo-input-item">
+            <label>Description</label>
+            <input type="text" placeholder="What's the description of your todo app?"/>
+          </div>
+          <div className="todo-input-item">
+            <button type="button" classname="primaryBtn">
+              Add
+            </button>
+          </div>
+        </div>
+
+        <div className="btn-area">
+          <button>Todo</button>
+          <button>Completed</button>
+        </div>
+
+        <div className="todo-list">
+          <div className="todo-list-item">
+            <h3>Task 1</h3>
+            <p>Description</p>
+          </div>
+        </div>
+
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
